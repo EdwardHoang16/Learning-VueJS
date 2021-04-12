398901f0657c0b712c9a1dd4bc8bc4814d5f7479
Edward Hoang <edward.hoang@sjsu.edu>
2021-04-12 14:20:21 -0700

    Started jobs website

---
+++ ninja-jobs/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, incidunt. Sed quaerat ad iure laboriosam dolore dolorum. Incidunt, aliquam, quisquam corporis excepturi assumenda quaerat aperiam, maxime consequuntur sed harum architecto. Quasi accusantium magnam modi laboriosam, dicta autem repudiandae libero veritatis saepe distinctio, assumenda illo quo nihil, enim commodi illum. Tenetur dignissimos quidem impedit? Magnam, corrupti numquam? Eius alias porro ab. Porro odio illo, consectetur odit ipsam placeat consequuntur, vero quos beatae assumenda, fugit deserunt. Autem fugiat eligendi molestiae architecto placeat exercitationem quaerat amet aliquid accusantium error earum quia itaque ipsa eaque, adipisci perspiciatis incidunt iusto cumque aspernatur. Architecto, praesentium veritatis!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, incidunt. Sed quaerat ad iure laboriosam dolore dolorum. Incidunt, aliquam, quisquam corporis excepturi assumenda quaerat aperiam, maxime consequuntur sed harum architecto. Quasi accusantium magnam modi laboriosam, dicta autem repudiandae libero veritatis saepe distinctio, assumenda illo quo nihil, enim commodi illum. Tenetur dignissimos quidem impedit? Magnam, corrupti numquam? Eius alias porro ab. Porro odio illo, consectetur odit ipsam placeat consequuntur, vero quos beatae assumenda, fugit deserunt. Autem fugiat eligendi molestiae architecto placeat exercitationem quaerat amet aliquid accusantium error earum quia itaque ipsa eaque, adipisci perspiciatis incidunt iusto cumque aspernatur. Architecto, praesentium veritatis!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: { }
+}
+</script>
