8cbecdab0f1248df3ba6e7efa0b7a18e0c7515e3
Edward Hoang <edward.hoang@sjsu.edu>
2021-04-13 21:19:20 -0700

    Added mark as complete and delete functionality

--- project-planner/src/components/SingleProject.vue
+++ project-planner/src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project">
-      <div class="actions" @click="toggleDetails">
-          <h3>{{ project.title }}</h3>
+  <div class="project" :class="{ complete: project.complete }">
+      <div class="actions">
+          <h3 @click="toggleDetails">{{ project.title }}</h3>
           <div class="icons">
               <span class="material-icons">edit</span>
-              <span class="material-icons">delete</span>
-              <span class="material-icons">done</span>
+              <span @click="deleteProject" class="material-icons">delete</span>
+              <span @click="toggleComplete" class="material-icons tick">done</span>
           </div>
       </div>
       <div class="details" v-if="showDetails">
@@ -19,12 +19,27 @@ export default {
     props: ['project'],
     data() {
         return {
-            showDetails: false
+            showDetails: false,
+            uri: 'http://localhost:3000/projects/' + this.project.id
         }
     },
     methods: {
         toggleDetails() {
             this.showDetails = !this.showDetails
+        },
+        deleteProject() {
+            fetch(this.uri, { method: 'DELETE' })
+                .then(() => this.$emit('delete', this.project.id))
+                .catch(err => console.log(err.message))
+        },
+        toggleComplete() {
+            fetch(this.uri, { 
+                method: 'PATCH', 
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ complete: !this.project.complete })
+            }).then(() => {
+            this.$emit('complete', this.project.id)
+            }).catch((err) => console.log(err.message))
         }
     }
 }
@@ -55,5 +70,11 @@ export default {
     }
     .material-icons:hover {
         color: #777;
+    }
+    .project.complete {
+        border-left: 4px solid #00ce89;
+    }
+    .project.complete .tick {
+        color: #00ce89;
     }
 </style>
