b3f24ce031971b1c40bf6f86b3b21f89706c5bc7
Edward Hoang <edward.hoang@sjsu.edu>
2021-04-14 20:16:52 -0700

    Finished project planner

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="current = $event" :current="current" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
@@ -10,13 +11,32 @@
 
 <script>
 import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
 
 export default {
   name: 'Home',
-  components: { SingleProject },
+  components: { SingleProject, FilterNav },
   data() {
     return {
-      projects: []
+      projects: [],
+      current: 'all'
+    }
+  },
+  computed: {
+    filteredProjects() {
+      let filteredProjects;
+      if (this.current === 'completed') {
+        filteredProjects = this.projects.filter(project => {
+          return project.complete === true
+        })
+      } else if (this.current === 'ongoing') {
+        filteredProjects = this.projects.filter(project => {
+          return project.complete === false
+        })
+      } else {
+        filteredProjects = this.projects
+      }
+      return filteredProjects
     }
   },
   mounted() {
