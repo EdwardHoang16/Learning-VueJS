fc6e0e79ecc4c31b0e6d1938e759b9899e6cf021
Edward Hoang <edward.hoang@sjsu.edu>
2021-04-15 19:36:55 -0700

    Started blog

---
+++ blog/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <PostList :posts="posts" />
+  </div>
+</template>
+
+<script>
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
+
+export default {
+  name: 'Home',
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1 },
+      { title: 'top 5 CSS tips', body: 'lorem ipsum', id: 2 }
+    ])
+
+    return { posts }
+  }
+}
+</script>
